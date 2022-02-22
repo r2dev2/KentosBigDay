bbb17dcef8dd5668cdf7a2f0ffe411b59aa5dd54
r2dev2 <ronak.badhe@gmail.com>
2022-02-22 14:56:46 -0800

    poem

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { Article } from 'spaper';
+</script>
+
+<main>
+  <div class="poem-container">
+    <Article
+      title="Happy 18th Birthday Kento Nishi!"
+      border
+    >
+      <p>You are now an adult.</p>
+      <p>Good job on expanding your cult.</p>
+      <p>Congrats for getting noticed by your oshi, Kiara.</p>
+      <p>In your deep voice, please tell me, <q>Ara Ara.</q></p>
+      <p>You must do it, <i>deus lo vult.</i></p>
+    </Article>
+  </div>
+</main>
+
+<style>
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+
+  .poem-container {
+    text-align: center;
+  }
+
+  p {
+    margin: 0.5rem 0 0 0;
+  }
+</style>
